a6b8ca722c238352a57b06a1198c0d5c0e19e755
Michael Fournier <4niermike@gmail.com>
2018-02-24 14:07:44 -0500

    successful first post

--- bot.js
+++ bot.js
@@ -4,3 +4,8 @@ var Twit = require('twit');
 
 var config = require('./config');
 var T = new Twit(config);
+
+
+T.post('statuses/update', { status: '' }, function(err, data, response) {
+  console.log('sent')
+})
