0f00f1804f3d9b51f83c26f50a9e7d597f75f1e2
Michael Fournier <4niermike@gmail.com>
2018-02-23 23:39:32 -0500

    First steps for Tweeting Bot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'lEa8CVaJTIq2NjBsJQmrcw5eJ',
+  consumer_secret:      'LldqhfBHgTozGsDMVDl6WteUpZIBwwNqwCJ97VUOWN0JPY3VXI',
+  access_token:         '967246730537431040-Q8tFlmeTKxjlg8EtAV86wdbdtPrvN5n',
+  access_token_secret:  'kp008d98iRoqloZRmpQkVigLLuG0b0i9nHXBhlJU9YD19'
+}
